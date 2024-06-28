364f312cfd37dc0237d05fa2043510ea930f2757
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2024-06-28 13:12:48 +0530

    Add files via upload

---
+++ practical4.java
@@ -0,0 +1,9 @@
+public class practical4{
+    public static void main(String[] args, String string) {
+        int[][] array={{1},{1,2},{1,2,3}};
+     
+        for(int i=0;i<array.length;i++){
+            System.out.println(string+ i +"] is :" + array[i].length);
+        }
+    }
+}
