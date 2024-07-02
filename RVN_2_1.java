5231eb73b371d23c8ef928f50f5b670694d9fd50
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2024-07-03 00:36:29 +0530

    Update RVN_2_1.java

--- RVN_2_1.java
+++ RVN_2_1.java
@@ -18,10 +18,10 @@ public class RVN_2_1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string");
+        System.out.println("Enter a string:");
           String str=sc.nextLine();
           
-     System.out.println("Enter number how many times you want to print");
+     System.out.println("Enter number how many times you want to print:");
      int num=sc.nextInt();
    sc.close();
     
