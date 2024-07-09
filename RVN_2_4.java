a313e8eddda48bca8bf5abbfa2cb6f4b9a390449
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2024-07-09 13:50:41 +0530

    Add files via upload

---
+++ RVN_2_4.java
@@ -0,0 +1,20 @@
+
+public class RVN_2_4 {
+
+    public static void main(String[] args) {
+
+        int i, n;
+        String str = "The";
+        // String  str2="AAbb";
+        //String  str3="Hi-There";
+        n = str.length();
+
+        for (i = 0; i < n; i++) {
+            char c = str.charAt(i);
+            System.out.print(c);
+            System.out.print(c);
+        }
+
+    }
+
+}
